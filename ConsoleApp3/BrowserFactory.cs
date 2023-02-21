b5df8f5cc98d62764783f8d1680fd69fa428ab01
wesam.mtour <wesam.mtour@nutanix.com>
2023-02-21 15:51:28 +0200

    Add project files.

---
+++ ConsoleApp3/BrowserFactory.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace ConsoleApp3
+{
+    internal class BrowserFactory
+    {
+        IWebDriver webDriver;
+        public void Init_Browser(String browserName)
+        {
+            switch (browserName.ToLower())
+            {
+                case "chrome":
+                    webDriver = new ChromeDriver();
+                    webDriver.Manage().Window.Maximize();
+                    break;
+            }
+        }
+        public string Title
+        {
+            get { return webDriver.Title; }
+        }
+        public void OpenUrl(string url)
+        {
+            webDriver.Url = url;
+        }
+        public IWebDriver getDriver
+        {
+            get { return webDriver; }
+        }
+        public void Close()
+        {
+            webDriver.Quit();
+        }
+    }
+}
---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace ConsoleApp3
+{
+    class NUnit_Demo_1
+    {
+        BrowserFactory brow = new BrowserFactory();
+        String test_url = "https://integration.vulcan.highcon.link/queue";
+        IWebDriver driver;
+
+        [SetUp]
+        public void start_Browser()
+        {
+            brow.Init_Browser("Chrome");
+        }
+
+        [Test]
+        public void test_Browserops()
+        {
+            brow.OpenUrl(test_url);
+            driver = brow.getDriver;
+            System.Threading.Thread.Sleep(4000);
+            IWebElement element = driver.FindElement(By.CssSelector("p#headerTitle"));
+            Console.WriteLine(element.Text);
+            // Test verification from the header title which should be equal to Job Queue
+            Assert.AreEqual(element.Text, "Job Queue");
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            brow.Close();
+        }
+    }
+}
